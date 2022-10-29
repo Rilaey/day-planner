42528d86baf6e2b59a820b012eaba93aba1a6bcb
Riley <94302477+Rilaey@users.noreply.github.com>
2022-10-29 18:10:52 -0400

    worked on html and css file. also used moment js to set date in the header

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,15 @@
-let currentDayEl = document.querySelector('#currentDay')
+let currentDayEl = document.querySelector('#currentDay');
 
 currentDayEl.innerHTML = moment().format("dddd MMM Do YYYY");
+
+let userInputEl = document.querySelector(".user-to-do");
+
+let timeOfDay = document.querySelector(".time-of-day");
+
+displayColors()
+
+function displayColors() {
+    if (timeOfDay < moment().format("hA")) {
+        userInputEl.attr("background-color", "black")
+    } 
+}    
